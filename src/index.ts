4b71379f920606e5af16ea0af0a9f2a6939feaba
Muhammad Bilal Shahid <muhammadbilalshahid@Muhammads-MacBook-Pro.local>
2026-05-20 14:30:55 +0500

    Added cors and get route query for subjects including few validations

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,27 @@
 import express from "express";
+import subjectsRoute from "./routes/subjects";
+import cors from 'cors';
 
 const app = express();
 const PORT = 8000;
 
+if(!process.env.FRONTEND_URL) {
+  throw new Error('Origin not found from env.')
+}
+
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  methods: ['GET', 'PUT', 'POST', 'DELETE'],
+  // For allowing cookies
+  credentials: true, 
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Classroom API is running.");
 });
+
+app.use("/api/subjects", subjectsRoute)
 
 app.listen(PORT, () => {
   console.log(`Server ready at http://localhost:${PORT}`);
